bb00ed4cd28761f2c981f4a0f230736bff6985f8
Sam <santiagoferreirati@gmail.com>
2023-11-14 15:02:00 -0300

    feito card global que renderizarar os produdos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import * as c from "./style";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Card from "../CardGeral/CardGeral";
+import Apple from '../img/apple-watch.png'
 function Home() {
   return (
     <>
       <Header />
-      <c.allContainers></c.allContainers>
+      <c.allContainers>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
+        
+      </c.allContainers>
       <Footer />
     </>
   );
