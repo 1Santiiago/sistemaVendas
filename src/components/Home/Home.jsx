49699df3fdecf65379e82360b2797e414e4f0e61
Sam <santiagoferreirati@gmail.com>
2023-11-14 17:31:03 -0300

    Fiz requisição via react-query. primeira vez que uso a tech

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,48 @@
-import React from "react";
 import * as c from "./style";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Card from "../CardGeral/CardGeral";
-import Apple from '../img/apple-watch.png'
+import { useQuery } from "react-query";
+
+
+
+
+const limit = 8
+const url = `https://api.mercadolibre.com/sites/MLB/search?q=iphone&limit=${limit}`;
+
 function Home() {
+
+  const { data, isLoading, error} = useQuery('items', async ()=>{
+    const response = await fetch(url)
+
+    if(!response.ok){
+      throw new Error('Erro na requisição')
+    }
+
+    const data = await response.json()
+   
+    return data.results
+
+  })
+
+  if(isLoading){
+    return console.log('carregando')
+  }
+
+  if (error) {
+    return <p>Erro ao buscar produtos: {error.message}</p>;
+  }
+  
+  
   return (
     <>
       <Header />
       <c.allContainers>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        <Card photo={Apple} title={'Apple Watch'} price={'999'}/>
-        
+       {
+        data.map((item, index)=>(
+          <Card key={index} photo={item.thumbnail} title={item.title} price={item.price}/>
+        ))
+       }
       </c.allContainers>
       <Footer />
     </>
