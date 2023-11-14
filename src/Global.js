52b6903c9a0eb78ccb153af03ed2ee196bf41feb
Sam <santiagoferreirati@gmail.com>
2023-11-14 13:44:24 -0300

    feito header e resete de estilos globais

---
+++ src/Global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+
+
+:root{
+    font-size: 62.5%;
+    --color-background: #1C1C1C;
+    --color-two: #ADFF2F;
+}
+  
+  *{
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+
+  body{
+    width: 100%;
+  }
+
+  `;
+
+
+
+
