fb9c26c1e4133f870e4d2d91e8d261a6a531d30f
Sam <santiagoferreirati@gmail.com>
2023-11-14 13:58:22 -0300

    footer pagina feito e estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import { GlobalStyle } from './Global';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header'
 function App() {
   return (
     <>
      <Header/>
+     <Footer />
      <GlobalStyle />
     </>
   );
---
+++ src/components/Footer/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const Footer = styled.footer`
+  background: #696969;
+  width: 100%;
+  display: flex;
+  align-items: baseline;
+  position: fixed;
+  bottom: 0;
+  justify-content: center;
+
+  > p{
+    font-size:1.6rem;
+    color:var(--color-two) 
+  }
+`;
