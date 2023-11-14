c8835ff692427ba93c94349715b5c9bc72ccdc6c
Sam <santiagoferreirati@gmail.com>
2023-11-14 14:34:29 -0300

    criado componente home para envolver a aplicação. sendo assim, deixo o app apenas com o home

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@
 import { GlobalStyle } from './Global';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header'
+import Home from './components/Home/Home';
 function App() {
   return (
     <>
-     <Header/>
-     <Footer />
+   
+    <Home />
      <GlobalStyle />
     </>
   );
